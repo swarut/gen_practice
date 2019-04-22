55ad048bdb3bb86ac026b47adfb58aa9422cb2c8
Warut Surapat <s.warut@gmail.com>
2019-04-23 01:49:39 +0900

    Use module attribute to store file content instead of macro.

--- lib/ruby_code_generator.ex
+++ lib/ruby_code_generator.ex
@@ -1,13 +1,12 @@
 defmodule RubyCodeGenerator do
   require EEx
 
-  defmacro get_file_content(atom_name, file_name) do
-    var = Macro.var(atom_name, nil)
-    {:ok, text} = File.read(file_name)
-    quote do
-      unquote(var) = unquote(text)
-    end
-  end
+  @main_file_content File.read("templates/ruby/ruby_base.eex")
+  @spec_file_content File.read("templates/ruby/ruby_spec_base.eex")
+  @gem_file_content File.read("templates/ruby/Gemfile")
+  @spec_config_file_content File.read("templates/ruby/.rspec")
+  @spec_helper_content File.read("templates/ruby/spec_helper.rb")
+
 
   def run(name, class_name) do
     IO.puts "Generating base Ruby code."
@@ -27,30 +26,30 @@ defmodule RubyCodeGenerator do
   end
 
   def gen_ruby_main_file(name, class_name) do
-    get_file_content(:ruby_main_file, "templates/ruby/ruby_base.eex")
-    ruby_main_file_output = EEx.eval_string(ruby_main_file, name: name, class_name: class_name)
+    {:ok, file_content} = @main_file_content
+    ruby_main_file_output = EEx.eval_string(file_content, name: name, class_name: class_name)
     File.write("ruby/#{name}.rb", ruby_main_file_output)
   end
 
   def gen_ruby_spec_file(name, class_name) do
-    get_file_content(:ruby_spec_file, "templates/ruby/ruby_spec_base.eex")
-    ruby_spec_file_output = EEx.eval_string(ruby_spec_file, name: name, class_name: class_name)
+    {:ok, file_content} = @spec_file_content
+    ruby_spec_file_output = EEx.eval_string(file_content, name: name, class_name: class_name)
     File.write("ruby/spec/#{name}_spec.rb", ruby_spec_file_output)
   end
 
   def gen_ruby_gem_file do
-    get_file_content(:ruby_gem_file, "templates/ruby/Gemfile")
-    File.write("ruby/Gemfile", ruby_gem_file)
+    {:ok, file_content} = @gem_file_content
+    File.write("ruby/Gemfile", file_content)
   end
 
   def gen_ruby_spec_config_file do
-    get_file_content(:ruby_spec_config_file, "templates/ruby/.rspec")
-    File.write("ruby/.rspec", ruby_spec_config_file)
+    {:ok, file_content} = @spec_config_file_content
+    File.write("ruby/.rspec", file_content)
   end
 
   def gen_ruby_spec_helper_file do
-    get_file_content(:ruby_spec_helper_file, "templates/ruby/spec_helper.rb")
-    File.write("ruby/spec/spec_helper.rb", ruby_spec_helper_file)
+    {:ok, file_content} = @spec_helper_content
+    File.write("ruby/spec/spec_helper.rb", file_content)
   end
 
 
